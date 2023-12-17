7df4546c1846deec6e3de8b52a8734d1f982dc34
zikotan <tananzakaria630@gmail.com>
2023-12-17 14:48:24 -0800

    response

---
+++ 3-quick_sort.c
@@ -0,0 +1,68 @@
+#include "sort.h"
+
+/**
+ * quick_sort - quick_sort function
+ * @array: arg1
+ * @size: arg2
+ *
+ * Return: Nothing.
+ **/
+void quick_sort(int *array, size_t size)
+{
+	size_t s0;
+	int *array0;
+
+	if (array)
+	{
+		array0 = array;
+		s0 = size;
+		rec(array0, s0, array, size);
+	}
+}
+
+/**
+ * rec - rec function
+ * @array0: arg1
+ * @s0: arg2
+ * @array: arg3
+ * @size: arg4
+ *
+ * Return: Nothing.
+ **/
+void rec(int *array0, size_t s0, int *array, size_t size)
+{
+	size_t i, to1;
+	int j = -1, n, p = array[size - 1];
+	int to2;
+
+	if (array && size > 1)
+	{
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i] < p)
+			{
+				j++;
+				to = j;
+				if (to1 != i && array[i] != array[to1])
+				{
+					n = array[i];
+					array[i] = array[j];
+					array[j] = n;
+					print_array(array0, s0);
+				}
+			}
+		}
+		to2 = size;
+		if (to2 - 1 != j + 1 && array[to2 - 1] != array[j + 1])
+		{
+			array[size - 1] = array[j + 1];
+			array[j + 1] = p;
+			print_array(array0, s0);
+		}
+		if (j > 0)
+		{
+			rec(array0, size0, array, j + 1);
+		}
+		rec(array0, size0, array + j + 2, size - (j + 2));
+	}
+}
--- sort.h
+++ sort.h
@@ -37,6 +37,8 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void rec(int *array0, size_t s0, int *array, size_t size);
 
 #endif
 
