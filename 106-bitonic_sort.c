b9e6d0710d1d121a0ca9eee286d1a1010e7656bb
zikotan <tananzakaria630@gmail.com>
2023-12-18 15:02:17 -0800

    response

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -63,12 +63,12 @@ void merge(int *array, size_t size, size_t begin, size_t s, char f)
 		for (i = begin; i < begin + c; i++)
 		{
 			if ((f == my_Up && array[i] > array[i + c])
-				|| (f == my_Down && array[i] < array[i + c]))
-				{
-					help = *(array + i);
-					*(array + i) = *(array + i + c);
-					*(array + i + c) = help;
-				}
+					|| (f == my_Down && array[i] < array[i + c]))
+			{
+				help = *(array + i);
+				*(array + i) = *(array + i + c);
+				*(array + i + c) = help;
+			}
 		}
 		merge(array, size, begin, c, f);
 		merge(array, size, begin + c, c, f);
