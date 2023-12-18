8a9c4b768382ee24c1222387616e5a21f17e2440
zikotan <tananzakaria630@gmail.com>
2023-12-17 16:48:55 -0800

    response

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -15,7 +15,7 @@ void counting_sort(int *array, size_t size)
 	if (!array || size < 2)
 		return;
 	n = array[0];
-	for (i = 1; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		if (array[i] > n)
 			n = array[i];
@@ -25,12 +25,13 @@ void counting_sort(int *array, size_t size)
 	{
 		c_Array[array[i]]++;
 	}
-	for (j = 1; j <= n; j++)
+	for (j = 1; j < n; j++)
 	{
 		c_Array[j + 1] += c_Array[j];
 	}
+	print_array(c_Array, n + 1);
 	to = malloc(sizeof(int) * size);
-	for (i = size - 1; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		c_Array[array[i]]--;
 		to[c_Array[array[i]]] = array[i];
